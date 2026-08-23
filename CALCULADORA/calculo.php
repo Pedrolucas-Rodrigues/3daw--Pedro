63512f16668714b339e8a343659a5a60058df3af
Pedrolucas-Rodrigues <rpedrolucas107@gmail.com>
2026-08-23 11:35:45 -0300

    Add files via upload

---
+++ CALCULADORA/calculo.php
@@ -0,0 +1,28 @@
+<?php 
+$num1 = $_GET["num1"];
+$num2 = $_GET["num2"];
+$op = $_GET["op"];
+
+switch($op){
+  case "+":
+    $result = $num1 + $num2;
+    echo"$result";
+    break;
+
+    case "-":
+    $result = $num1 - $num2;
+    echo"$result";
+    break;
+
+    case "*":
+    $result = $num1 * $num2;
+    echo"$result";
+    break;
+
+    case "/":
+    $result = $num1 * $num2;
+    echo"$result";
+    break;
+
+}
+?>
